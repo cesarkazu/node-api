3d73e5ae648525e1c93feb0f6e6f74339ca13f17
Cesar Kazuyoshi Ando <ckando@stefanini.com>
2020-02-27 09:19:02 -0300

    criando-model-de-produto

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // Iniciando o App
 const app = express();
@@ -12,10 +13,19 @@ mongoose.connect(
         useUnifiedTopology: true
     }
 );
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 
 // Primeira rota
 app.get('/', (req, res) => {
-    res.send('Hello Rocketseat');
-})
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native'
+    });
+
+    return res.send('Hello Rocketseat');
+});
 
 app.listen(3001);
