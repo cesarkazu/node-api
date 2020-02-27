33e2c47e07c3216c5b6759b40b8cb1bcb516f40d
Cesar Kazuyoshi Ando <ckando@stefanini.com>
2020-02-27 15:23:56 -0300

    reestruturacao-de-arquivos

--- server.js
+++ server.js
@@ -15,17 +15,7 @@ mongoose.connect(
 );
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-// Primeira rota
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build native apps with React',
-        url: 'http://github.com/facebook/react-native'
-    });
-
-    return res.send('Hello Rocketseat');
-});
+// Rotas
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
